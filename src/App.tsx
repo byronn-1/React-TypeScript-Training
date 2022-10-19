6e95bca07f42fb090a29266629cb02c00d51ec40
Byron Lee <byronn.lee@gmail.com>
2022-10-19 13:36:37 +0100

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import './App.css';
 
 function App() {
 
+  // let firstValue: string = 'Hiya';
+  // let firstValue: number = 23;
+  // let firstValue: boolean = false;
+  // let firstValue: number[] = [1, 2, 3, 4, 5, 6, 7];
+  // OR.......
+  let firstValue: Array<number> = [1, 2, 3, 4, 5, 6, 7];
+  //REMEMBER:  in TS arrays are of object type!
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Nothing to see here!
+        The value { firstValue } is of { typeof firstValue} type!
         </p>
       </header>
     </div>
